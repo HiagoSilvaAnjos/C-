ce806a93ec46bd23a78984d3624d0172bff339db
Hialagol <hiagosilvaanjos7@gmail.com>
2025-04-29 10:53:37 -0300

    chore: Update tasks.json for C/C++ build configuration

--- exec.cpp
+++ exec.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// cout => print to console
+// variable => type and name and value => int age = 20;
+// endl => new line
+// cin => input from console
+
 int main() {
-  cout << "Hello World!";
-  return 0;
+  int age;
+  string name;
+  float height;
+  int width;
+
+  cout << "Enter your name and age: " << endl;
+
+  cin >> name;
+  cin >> age;
+  cin >> height >> width;
+
+  cout << "Hello " << name << ", you are " << age << " years. " << height << " " << width << endl;
+  cout << endl;
+
+  if (age >= 18) {
+    cout << name << " age >= 18" << endl;
+  } else if (age >= 16) {
+    cout << "age >= 16" << endl;
+  }
+   else {
+    cout << "age < 16" << endl;
+   }
+
+
+  return 1;
 }
