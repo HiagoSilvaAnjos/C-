c6161c91b423190228256aeafd8eaac550222ced
Hialagol <hiagosilvaanjos7@gmail.com>
2025-05-27 11:04:42 -0300

    feat: Disable error squiggles in C/C++ settings

---
+++ aulas/Aula06/exercicio/exec.cpp
@@ -0,0 +1,76 @@
+#include <iostream> 
+using namespace std; 
+
+struct memes{
+    int num_meme;
+    string nome_arq, assunto, site;
+};
+
+void selection_sort(memes vetor[], int tam) {
+    int menor;
+    memes aux_troca;
+
+    for (int indice = 0; indice < tam - 1; indice++) {
+        // `indice` é o local onde o menor elemento da rodada será alocado
+        menor = indice;
+
+        // Encontrar o menor elemento no restante do vetor
+        for (int j = indice + 1; j < tam; j++) { // Corrigido: `j < tam`
+            if (vetor[j].num_meme < vetor[menor].num_meme) { // Corrigido: `<` para encontrar o menor
+                menor = j;
+            }
+        }
+
+        // Trocar o menor elemento encontrado com o elemento na posição `indice`
+        if (menor != indice) {
+            aux_troca = vetor[indice];
+            vetor[indice] = vetor[menor];
+            vetor[menor] = aux_troca;
+        }
+    }
+}
+
+
+void entrada (memes v[], int total){
+    cout << "Digite os dados dos memes:" << endl;
+    for (int i = 0; i<total;i++){
+        cin>>v[i].num_meme>>v[i].nome_arq;
+        cin>>v[i].assunto>>v[i].site;
+    }
+}
+
+void imprimi (memes v[], int total){
+    cout << "Memes cadastrados:" << endl;
+    for (int i = 0; i<total;i++){
+        cout<<v[i].num_meme<<" "<<v[i].nome_arq<<" ";
+        cout<<v[i].assunto<<" "<<v[i].site<<endl;
+    }
+}
+
+int main(){
+    int tamanho;
+    string busca;
+    cin>>tamanho;
+    memes v[tamanho];
+
+    entrada(v,tamanho);
+
+    cout << "Digite o site que deseja buscar: ";
+    cin >>busca;
+
+    for (int i = 0; i < tamanho; i++) {
+        
+        if(v[i].site == busca) {
+            selection_sort(v, tamanho);
+        }
+
+    }
+
+    cout << endl;
+
+    imprimi(v,tamanho);
+
+    //selection_sort(v,tamanho);
+
+    return 0;
+}
